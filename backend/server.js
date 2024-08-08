36ee58c8e2889854851dd3c94cd5cb1a6392b697
Sahir Momin <sahirmomin65@gmail.com>
2024-08-08 12:52:37 -0600

    CRUD api completed and tested for notes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
@@ -14,11 +15,12 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
